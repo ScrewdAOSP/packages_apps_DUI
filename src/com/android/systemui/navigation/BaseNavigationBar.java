f44ec52c01422370654e56fe1f99f796637ae931
bigrushdog <randall.rushing@gmail.com>
2016-02-26 11:40:41 -0800

    SmartBar: Battery Bar support [2/2]
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- src/com/android/systemui/navigation/BaseNavigationBar.java
+++ src/com/android/systemui/navigation/BaseNavigationBar.java
@@ -524,10 +524,10 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
         mRot90.setPadding(mRot90.getPaddingLeft(), 0, mRot90.getPaddingRight(),
                 mRot90.getPaddingBottom());
 
-//        addBatteryBarLayout(mRot0);
+        addBatteryBarLayout(mRot0);
         mRot0.addView(rot0NavButton);
 
-//        addBatteryBarLayout(mRot90);
+        addBatteryBarLayout(mRot90);
         mRot90.addView(rot90NavButton);
 
         addView(mRot0);
@@ -539,24 +539,26 @@ public abstract class BaseNavigationBar extends LinearLayout implements Navigato
         mRotatedViews[Surface.ROTATION_270] = mRotatedViews[Surface.ROTATION_90];
         mCurrentView = mRotatedViews[Surface.ROTATION_0];
     }
-/*
-    private void addBatteryBarLayout(ViewGroup parent) {
-        int which = -1;
+
+   private void addBatteryBarLayout(ViewGroup parent) {
+        String name = null;
         if (parent.equals(mRot0)) {
-            which = R.layout.battery_bar_rot0;
+            name = "battery_bar_rot0";
         } else if (parent.equals(mRot90)) {
-            which = R.layout.battery_bar_rot90;
+            name = "battery_bar_rot90";
         } else {
             return;
         }
         try {
-            View bar = View.inflate(getContext(), which, null);
+            View bar = View.inflate(getContext(),
+                    DUActionUtils.getIdentifier(getContext(), name,
+                            "layout", DUActionUtils.PACKAGE_SYSTEMUI), null);
             parent.addView(bar);
         } catch (Exception e) {
             Log.e(TAG, "BatteryBarController failed to inflate");
         }
     }
-*/
+
     protected void setVisibleOrGone(View view, boolean visible) {
         if (view != null) {
             view.setVisibility(visible ? VISIBLE : GONE);
